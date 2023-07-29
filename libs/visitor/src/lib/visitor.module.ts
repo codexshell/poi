c6c3e93417292c4d3f0b267401238ad59f9cfac5
codexshell <ericoderito@gmail.com>
2023-07-29 19:51:28 +0300

    chore: integrate the `visitor` lib into the application

--- libs/visitor/src/lib/visitor.module.ts
+++ libs/visitor/src/lib/visitor.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VisitorComponent } from './visitor/visitor.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+import { VisitorComponent } from './visitor/visitor.component';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
-  declarations: [
-    VisitorComponent
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: VisitorComponent,
+      },
+    ]),
   ],
+  declarations: [VisitorComponent],
 })
 export class VisitorModule {}
